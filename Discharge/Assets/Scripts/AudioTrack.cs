a623a868ed043906f572987efc8b08d5c6b615fb
Giovanni <Gio@Hastur.student.rit.edu>
2017-11-28 11:43:29 -0500

    Updated Audio Script

--- Discharge/Assets/Scripts/AudioTrack.cs
+++ Discharge/Assets/Scripts/AudioTrack.cs
@@ -6,55 +6,56 @@ public class AudioTrack : MonoBehaviour {
 
 	private AudioSource[] tracks;
 
-    public int TrackSize
-    { 
-        get {
-            return tracks.Length;
-        }
-    }
-    
-        
+	public int TrackSize
+	{
+		get {
+			return tracks.Length;
+		}
+	}
 
 	public enum State {t1, t2, t3};
 
-    private State currState = State.t1;
+	private State currState = State.t1;
 
-    public State CurrState
-    {
-        get { return currState; }
-        set { currState = value; }
-    }
+	public State CurrState
+	{
+		get { return currState; }
+		set { currState = value; }
+	}
 
 	// Use this for initialization
 	void Start () {
 		tracks = GetComponents<AudioSource>();
+		tracks[0].loop = true;
 		tracks[0].Play();
 		tracks[1].mute = true;
+		tracks[1].loop = true;
 		tracks[1].Play();
 		tracks[2].mute = true;
+		tracks[2].loop = true;
 		tracks[2].Play();
 	}
 
 	// Update is called once per frame
 	void Update () {
-        switch (currState)
-        {
-            case (State.t1):
-                tracks[1].mute = true;
-                tracks[2].mute = true;
-                break;
-            case (State.t2):
-                tracks[1].mute = false;
-                tracks[2].mute = true;
-                break;
-            case (State.t3):
-                tracks[1].mute = false;
-                tracks[2].mute = false;
-                break;
-            default:
-                tracks[1].mute = true;
-                tracks[2].mute = true;
-                break;
+		switch (currState)
+		{
+			case (State.t1):
+				tracks[1].mute = true;
+				tracks[2].mute = true;
+				break;
+			case (State.t2):
+				tracks[1].mute = false;
+				tracks[2].mute = true;
+				break;
+			case (State.t3):
+				tracks[1].mute = false;
+				tracks[2].mute = false;
+				break;
+			default:
+				tracks[1].mute = true;
+				tracks[2].mute = true;
+				break;
         }
 	}
 }
